9e72db24d6bc0c785c5a49401e8ccef30ff97558
benjamistan <ben@benstanley.pro>
2022-01-27 08:51:36 +0100

    add creator-dashboard

--- pages/creator-dashboard.tsx
+++ pages/creator-dashboard.tsx
@@ -8,8 +8,9 @@ import { nftmarketaddress, nftaddress } from '../config';
 import Market from '../artifacts/contracts/Market.sol/NFTMarket.json';
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
 
-export default function MyAssets() {
+export default function CreatorDashboard() {
 	const [nfts, setNfts] = useState<Token[]>([]);
+	const [sold, setSold] = useState<Token[]>([]);
 	const [loadingState, setLoadingState] = useState('not-loaded');
 
 	interface Token {
@@ -21,6 +22,7 @@ export default function MyAssets() {
 		image: string;
 		name: string;
 		description: string;
+		sold: boolean;
 	}
 
 	useEffect(() => {
@@ -42,9 +44,9 @@ export default function MyAssets() {
 			signer
 		);
 		const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
-		const data = await marketContract.fetchMyNFTs();
+		const data = await marketContract.fetchItemsCreated();
 
-		const items: Token[] = await Promise.all(
+		const items = await Promise.all(
 			data.map(async (i: Token) => {
 				const tokenUri = await tokenContract.tokenURI(i.tokenId);
 				const meta = await axios.get(tokenUri);
@@ -54,20 +56,24 @@ export default function MyAssets() {
 					tokenId: i.tokenId.toNumber(),
 					seller: i.seller,
 					owner: i.owner,
+					sold: i.sold,
 					image: meta.data.image,
 				};
 				return item;
 			})
 		);
+		/* create a filtered array of items that have been sold */
+		const soldItems = items.filter((i) => i.sold);
+		setSold(soldItems);
 		setNfts(items);
 		setLoadingState('loaded');
 	}
-
 	if (loadingState === 'loaded' && !nfts.length)
-		return <h1 className='py-10 px-20 text-3xl'>No assets owned</h1>;
+		return <h1 className='py-10 px-20 text-3xl'>No assets created</h1>;
 	return (
-		<div className='flex justify-center'>
+		<div>
 			<div className='p-4'>
+				<h2 className='text-2xl py-2'>Items Created</h2>
 				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
 					{nfts.map((nft, i) => (
 						<div key={i} className='border shadow rounded-xl overflow-hidden'>
@@ -80,6 +86,28 @@ export default function MyAssets() {
 						</div>
 					))}
 				</div>
+			</div>
+			<div className='px-4'>
+				{Boolean(sold.length) && (
+					<div>
+						<h2 className='text-2xl py-2'>Items sold</h2>
+						<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+							{sold.map((nft, i) => (
+								<div
+									key={i}
+									className='border shadow rounded-xl overflow-hidden'
+								>
+									<img src={nft.image} className='rounded' />
+									<div className='p-4 bg-black'>
+										<p className='text-2xl font-bold text-white'>
+											Price - {nft.price} Eth
+										</p>
+									</div>
+								</div>
+							))}
+						</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
