85e94446d2599073964b111238ec4f3346f827bc
benjamistan <ben@benstanley.pro>
2022-02-02 18:48:27 +0100

    refactor:component structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,35 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
+import Image from 'next/image';
+
+import Header from '../components/Header';
+
+import logo from '../assets/Logo.svg';
 
 function Marketplace({ Component, pageProps }: AppProps) {
 	return (
 		<div>
-			<nav className='border-b p-6'>
-				<p className='text-4xl font-bold'>NFTM</p>
-				<div className='flex mt-4'>
-					<Link href='/'>
-						<a className='mr-4 text-pink-500'>Home</a>
-					</Link>
-					<Link href='/create-item'>
-						<a className='mr-6 text-pink-500'>Mint</a>
-					</Link>
-					<Link href='/dashboard'>
-						<a className='mr-6 text-pink-500'>Dashboard</a>
-					</Link>
-				</div>
-			</nav>
-			<Component {...pageProps} />
+			<Header />
 		</div>
 	);
 }
 
 export default Marketplace;
+
+// nav className = 'border-b p-6' >
+// 				<div className='flex mt-4'>
+// 					<Link href='/' passHref>
+// 						<a>
+// 							<Image src={logo} alt='Ownr logo' height={50} width={150}></Image>
+// 						</a>
+// 					</Link>
+// 					<Link href='/create-item'>
+// 						<a className='mr-6 text-pink-500'>Mint</a>
+// 					</Link>
+// 					<Link href='/dashboard'>
+// 						<a className='mr-6 text-pink-500'>Dashboard</a>
+// 					</Link>
+// 				</div>
+// 			</nav>
+// 			<Component {...pageProps} />
