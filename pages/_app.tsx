d304f4d5e637fa2281db4d7273142da349e2198b
benjamistan <ben@benstanley.pro>
2022-02-14 09:17:42 +0100

    feat:add footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import Head from 'next/head';
 
 import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
 
+import Footer from '../components/Footer';
+
 const supportedChainIds = [80001];
 const connectors = {
 	injected: {},
@@ -20,6 +22,9 @@ function Marketplace({ Component, pageProps }: AppProps) {
 					<title>Ownr NFT Marketplace</title>
 				</Head>
 				<Component {...pageProps} />
+			</div>
+			<div>
+				<Footer />
 			</div>
 		</ThirdwebWeb3Provider>
 	);
