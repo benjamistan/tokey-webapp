0abf6c0f323ff65cc832a1b6404cd96c9375fdda
benjamistan <ben@benstanley.pro>
2022-03-20 18:37:03 +0100

    centred Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -10,10 +10,10 @@ import { CgProfile } from 'react-icons/cg';
 import { MdOutlineAccountBalanceWallet } from 'react-icons/md';
 
 const style = {
-	wrapper: `flex bg-[#034078] w-[screen] px-[1.2rem] py-[0.8rem] `,
+	container: `flex justify-center bg-[#034078] w-[screen] px-[1.2rem] py-[0.8rem] `,
 	logoContainer: `flex items-center cursor-pointer`,
 	logoText: ` ml-[0.8rem] text-white font-semibold text-2xl`,
-	searchBar: `flex flex-auto mx-[0.8rem] w-max-[520px] items-center bg-[#e5e5e5] rounded-[0.8rem] hover:bg-[#fefcfb]`,
+	searchBar: `flex flex-none mx-[0.8rem] xl:w-[820px] lg:w-[520px] md:w-[420px] sm:w-[120px] items-center bg-[#e5e5e5] rounded-[0.8rem] hover:bg-[#fefcfb]`,
 	searchIcon: `text-[#000] mx-3 font-bold text-lg`,
 	searchInput: `h-[2.6rem] w-full border-0 bg-transparent outline-0 ring-0 px-2 pl-0 text-[#000] placeholder:text-[#8a939b]`,
 	searchButton: `border bg-[#2081e2] font-semibold44sdw cursor-pointer text-black`,
@@ -24,7 +24,7 @@ const style = {
 
 const Header = () => {
 	return (
-		<div className={style.wrapper}>
+		<div className={style.container}>
 			<Link href='/' passHref>
 				<div className={style.logoContainer}>
 					<Image
