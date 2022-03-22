9a425b6f01a17bf91b72c039b5ba4f6394746da9
benjamistan <ben@benstanley.pro>
2022-03-22 20:44:36 +0100

    style:improved footer styling

--- components/Footer/FooterLinks.tsx
+++ components/Footer/FooterLinks.tsx
@@ -1,56 +1,48 @@
 import React from 'react';
 import Link from 'next/link';
 
-const style = {
-	footer: `bg-[#034078] text-white relative justify-center flex flex-wrap grid grid-cols-5 items-center text-left pb-20`,
-	link: `text-white hover:text-[#a7b0c4]`,
-	linksColHeader: `font-bold text-xl mb-4 border-b`,
-	linksList: `list-non mb-0`,
-	linksListItem: `text-white`,
-};
-
 const FooterLinks = () => {
 	return (
-		<div className={style.footer}>
+		<div className='grid grid-cols-5 justify-center bg-[#034078] text-white relative items-center pb-20'>
 			<div>&nbsp;</div>
-			<div>
-				<h5 className={style.linksColHeader}>Assets</h5>
-				<ul className={style.linksList}>
-					<li className={style.link}>
+			<div className='pl-20'>
+				<h5 className='font-bold text-xl mb-4 border-b'>Assets</h5>
+				<ul className='list-non mb-0'>
+					<li className='text-white hover:text-[#a7b0c4] cursor-pointer'>
 						<Link href='/collections'>Collections</Link>
 					</li>
-					<li className={style.link}>
-						<Link href='/create-item'>Mint</Link>
+					<li className='text-white hover:text-[#a7b0c4] cursor-pointer'>
+						<Link href='/create'>Create</Link>
 					</li>
-					<li className={style.link}>
+					<li className='text-white hover:text-[#a7b0c4] cursor-pointer'>
 						<Link href='/dashboard'>Dashboard</Link>
 					</li>
 				</ul>
 			</div>
-			<div>
-				<h5 className={style.linksColHeader}>Resources</h5>
-				<ul className={style.linksList}>
-					<li className={style.link}>
+			<div className='pl-20'>
+				<h5 className='font-bold text-xl mb-4 border-b'>Resources</h5>
+				<ul className='list-non mb-0'>
+					<li className='text-white hover:text-[#a7b0c4] cursor-pointer'>
 						<Link href='/'>Help Centre</Link>
 					</li>
-					<li className={style.link}>
+					<li className='text-white hover:text-[#a7b0c4] cursor-pointer'>
 						<Link href='/'>Platform Status</Link>
 					</li>
-					<li className={style.link}>
+					<li className='text-white hover:text-[#a7b0c4] cursor-pointer'>
 						<Link href='/'>Partners</Link>
 					</li>
 				</ul>
 			</div>
-			<div>
-				<h5 className={style.linksColHeader}>Company</h5>
-				<ul className={style.linksList}>
-					<li className={style.link}>
+			<div className='pl-20'>
+				<h5 className='font-bold text-xl mb-4 border-b'>Company</h5>
+				<ul className='list-non mb-0'>
+					<li className='text-white hover:text-[#a7b0c4] cursor-pointer'>
 						<Link href='/collections'>Who we are</Link>
 					</li>
-					<li className={style.link}>
+					<li className='text-white hover:text-[#a7b0c4] cursor-pointer'>
 						<Link href='/privacy'>Privacy Policy</Link>
 					</li>
-					<li className={style.link}>
+					<li className='text-white hover:text-[#a7b0c4] cursor-pointer'>
 						<Link href='/termsofservice'>Terms of Service</Link>
 					</li>
 				</ul>
--- components/Footer/NewsletterSignup.tsx
+++ components/Footer/NewsletterSignup.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 
 const style = {
-	footer: `bg-[#034078] w-screen px-[1.2rem] py-[0.8rem] pt-6 flex text-white relative justify-center flex-wrap items-center mb-10`,
-	form: `ml-[0.8rem] grid md:grid-cols-3 gird-cols-1 gap-4 flex justify-center items-center text-white`,
-	formCTA: `font-bold text-3xl`,
-	formInput: `form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none`,
+	form: `ml-[0.8rem] grid md:grid-cols-3 grid-cols-1 gap-4 flex justify-center items-center text-white`,
+	formInput: ``,
 	formButton: `inline-block px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out`,
-	button: `inline-block relative font-semibold px-12 py-2 bg-[#363840] rounded mr-5 text-[#e4e8ea] hover:bg-[#4c505c] cursor-pointer`,
 };
 
 const NewsletterSignup = () => {
 	return (
-		<div className={style.footer}>
+		<div className='bg-[#034078] w-screen px-[1.2rem] py-[0.8rem] pt-6 flex text-white relative justify-center flex-wrap items-center mb-10'>
 			<div className={style.form}>
-				<div className={style.formCTA}>Join mailing list:</div>
+				<div className='font-bold text-3xl pt-4'>Join mailing list:</div>
 				<div>
-					<input className={style.formInput} placeholder='Your email address' />
+					<input
+						className='form-control block w-full px-3 py-1.5 pt-4 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+						placeholder='Your email address'
+					/>
 				</div>
 				<div>
-					<button className={style.button} type='submit'>
+					<button
+						className='inline-block font-semibold px-10 align-middle py-2 pt-4 bg-[#363840] rounded mr-5 text-[#e4e8ea] hover:bg-[#4c505c] cursor-pointer'
+						type='submit'
+					>
 						Submit
 					</button>
 				</div>
