7dea87b1b17738183e582627408ffe0aabfb3489
benjamistan <ben@benstanley.pro>
2022-03-22 07:36:47 +0100

    finish hero

--- components/HomePage/Hero.tsx
+++ components/HomePage/Hero.tsx
@@ -6,10 +6,19 @@ import HeroNFTCard from './HeroNFTCard';
 
 function Hero() {
 	return (
-		<div className='flex justify-center align-middle w-screen'>
-			<div className='flex justify-center container mx-auto'>
+		<div className='flex justify-center align-middle w-screen h-[600px]'>
+			<div className='flex justify-center container mx-auto pt-8'>
 				<div className='p-10 w-2/5 mt-10'>
-					<p className='text-3xl font-bold'>What will you collect today?</p>
+					<p className='text-purple-500 uppercase font-light'>
+						tiny text in caps
+					</p>
+					<p className='text-5xl font-bold pb-5'>
+						What will you collect today?
+					</p>
+					<p className='text-2xl'>
+						Lorem ipsum dolor sit amet. Aut impedit sunt aut ipsa dolor est
+						rerum asperiores aut deleniti illo?{' '}
+					</p>
 					<div className='space-x-2'>
 						<Link href='/' passHref>
 							<button className='rounded mt-5 p-2 px-5 bg-gray-400 hover:bg-gray-300 transition-all ease-in text-xl'>
@@ -24,26 +33,30 @@ function Hero() {
 					</div>
 				</div>
 
-				<div className='flex w-2/5 p-10 mt-10 space-x-2'>
+				<div className='flex w-4/5 p-10 mt-10 space-x-2'>
 					<HeroNFTCard
 						image='ape1.png'
-						collection='Bored Ape Yacht Club'
-						nftName='8337'
+						collection='Fancy Dans'
+						nftName='Dave'
+						price='43'
 					/>
 					<HeroNFTCard
 						image='ape2.png'
-						collection='Bored Ape Yacht Club'
-						nftName='8337'
+						collection='Fancy Dans'
+						nftName='John'
+						price='56'
 					/>
 					<HeroNFTCard
 						image='ape3.png'
-						collection='Bored Ape Yacht Club'
-						nftName='8337'
+						collection='Fancy Dans'
+						nftName='Maud'
+						price='34'
 					/>
 					<HeroNFTCard
 						image='ape4.png'
-						collection='Bored Ape Yacht Club'
-						nftName='8337'
+						collection='Fancy Dans'
+						nftName='Bruno'
+						price='61'
 					/>
 				</div>
 			</div>
--- components/HomePage/HeroNFTCard.jsx
+++ components/HomePage/HeroNFTCard.jsx
@@ -3,22 +3,29 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const HeroNFTCard = ({ image, collection, nftName }) => {
+import { FaEthereum } from 'react-icons/fa';
+import { SiEthereum } from 'react-icons/si';
+
+const HeroNFTCard = ({ image, collection, nftName, price }) => {
 	console.log('Collection:', { collection });
 	return (
-		<div className='border-2 shadow-xl rounded-[12px]'>
+		<div className='border-2 shadow-xl rounded-[12px] h-min'>
 			<Image src={image} alt='An NFT Image' width={250} height={250} />
-			<div className='flex justify-between p-1'>
+			<div className='flex justify-between px-1'>
 				<div className='font font-medium hover:text-gray-600 transition-all ease-in'>
 					<Link href='/collections'>{collection}</Link>
 				</div>
-				<div className='font-bold hover:text-gray-600 transition-all ease-in'>
-					<Link href='/nft'>99 ETH</Link>
+				<div className='flex align-middle font-bold hover:text-gray-600 transition-all ease-in'>
+					<Link href='/nft'>{price}</Link>
+					<div className='pt-1 text-[#0d559d]'>
+						<SiEthereum />
+					</div>
 				</div>
 			</div>
 			<div className='font-bold text-[#0d559d] hover:text-blue-300 transition-all ease-in px-1'>
 				<Link href='/nft'>{nftName}</Link>
 			</div>
+			<div className='border-b-2 border-b-gray-200 pt-4 mb-4 mx-2'></div>
 		</div>
 	);
 };
