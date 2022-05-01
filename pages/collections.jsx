a038f657ef08dfec6a936737423b4ab9734a92f3
benjamistan <ben@benstanley.pro>
2022-05-01 10:31:48 +0100

    upgrade node and renew package-lock

---
+++ pages/collections.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Assets } from '../components/HomePage';
+
+const collections = () => {
+	const collectionId = '0x4b94B8077da9db887a37a8814dAc0CFAD22B5A99';
+
+	return (
+		<div>
+			<Assets />
+		</div>
+	);
+};
+
+export default collections;
