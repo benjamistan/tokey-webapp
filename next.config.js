5cc71cb1461bd7b644a10476b9e2da67b13c1808
benjamistan <ben@benstanley.pro>
2022-06-10 14:33:24 +0200

    feat: added AWS access keys to env.local

--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
 	env: {
 		PASSWORD_PROTECT: true,
 		AWS_REGION: 'eu-west-2',
+		ENVIRONMENT: process.env.ENVIRONMENT || 'production',
+		STAGING_AWS_ACCESS_KEY_ID: process.env.STAGING_AWS_ACCESS_KEY_ID,
+		STAGING_AWS_SECRET_ACCESS_KEY: process.env.STAGING_AWS_SECRET_ACCESS_KEY,
 	},
 	webpack(config) {
 		config.module.rules.push({
