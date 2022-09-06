4a46e5a152f2822a7b4288de8dc90dbcac7f0eac
benjamistan <ben@benstanley.pro>
2022-09-06 13:29:11 +0200

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { Toaster } from 'react-hot-toast';
-import { Assets } from '../components/AssetView';
 
 const style = {
   container: 'flex text-center bg-white pb-40 pl-10',
@@ -10,9 +9,7 @@ export default function Home() {
   return (
     <div className={style.wrapper}>
       <Toaster position="bottom-right" reverseOrder={false} />
-      <div className={style.container}>
-        <Assets />
-      </div>
+      <div className={style.container}></div>
     </div>
   );
 }
