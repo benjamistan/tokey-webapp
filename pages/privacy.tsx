9181063875e376fb43b48361394a1e575b7761af
benjamistan <ben@benstanley.pro>
2022-09-10 15:16:21 +0200

    refactor:remove unfinished pages

--- pages/privacy.tsx
+++
@@ -1,9 +0,0 @@
-import PrivacyPolicy from '../components/PrivacyPolicy/PrivacyPolicy';
-
-export default function Privacy() {
-  return (
-    <div>
-      <PrivacyPolicy />
-    </div>
-  );
-}
--- pages/termsofservice.tsx
+++
@@ -1,9 +0,0 @@
-import TermsOfService from '../components/TermsOfService/TermsOfService';
-
-export default function Privacy() {
-  return (
-    <div>
-      <TermsOfService />
-    </div>
-  );
-}
