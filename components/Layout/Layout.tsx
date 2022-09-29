16d27f600eb7fd76a31b5fa00d7ebbd4939e5287
benjamistan <ben@benstanley.pro>
2022-09-29 11:46:14 +0200

    feat:refined wallet connector

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -10,10 +10,11 @@ const user = {
   imageUrl:
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 };
+
 const navigation = [
   { name: 'Dashboard', href: '/dashboard' },
   { name: 'Create', href: '/create' },
-  { name: 'Exchange', href: '/collections' },
+  { name: 'Exchange', href: '#' },
   { name: 'Performance', href: '#' },
 ];
 const userNavigation = [
--- components/WalletConnector/WalletConnectDropdown.jsx
+++ components/WalletConnector/WalletConnectDropdown.jsx
@@ -5,6 +5,7 @@ import { Fragment } from 'react';
 import {
   useAddress,
   useCoinbaseWallet,
+  useDisconnect,
   useMetamask,
   useNetwork,
   useWalletConnect,
@@ -16,6 +17,7 @@ export default function WalletConnectDropdown() {
   const connectWithWalletConnect = useWalletConnect();
   const address = useAddress();
   const network = useNetwork();
+  const disconnectWallet = useDisconnect();
 
   const chainz = {
     1: 'Eth Mainnet',
@@ -27,20 +29,22 @@ export default function WalletConnectDropdown() {
 
   if (address) {
     return (
-      <div className="w-46 text-right text-white text-sm mt-2">
-        <Menu as="div" className="inline-block text-left">
-          <div className="block font-semibold">
-            Address:{' '}
-            <div className="inline font-thin">
-              {address.substring(0, 6) + '...'}
+      <div className="flex text-right text-black text-sm mt-2">
+        <Menu>
+          <Menu as="div" className="inline-block text-left">
+            <div className="block font-semibold">
+              Address:{' '}
+              <div className="inline font-thin">
+                {address.substring(0, 6) + '...'}
+              </div>
             </div>
-          </div>
-          <div className="font-semibold">
-            Chain:{' '}
-            <div className="inline font-thin">
-              {network[0].data.chain && chainz[network[0].data.chain.id]}
+            <div className="font-semibold">
+              Chain:{' '}
+              <div className="inline font-thin">
+                {network[0].data.chain && chainz[network[0].data.chain.id]}
+              </div>
             </div>
-          </div>
+          </Menu>
         </Menu>
       </div>
     );
@@ -49,7 +53,7 @@ export default function WalletConnectDropdown() {
   return (
     <div className="ml-10 space-x-4">
       <Menu as="div" className="relative inline-block text-left">
-        <Menu.Button className="inline-flex justify-center w-full px-4 py-2 font-medium text-[#F7F7FF] bg-tblue rounded-md  hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+        <Menu.Button className="inline-flex justify-center w-full px-4 py-2 font-medium text-sm text-[#F7F7FF] bg-tblue rounded-md  hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
           Connect Wallet
           <ChevronDownIcon
             className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
